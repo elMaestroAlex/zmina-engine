3568dd16ae4dde5bd38647bbcdca1ff352fef613
Alexandr Baranetsky <asterai.com@gmail.com>
2022-11-27 19:53:50 +0200

    Added logic

--- src/main/java/com/asterai/zmina/processor/BaseLogicProcessor.java
+++ src/main/java/com/asterai/zmina/processor/BaseLogicProcessor.java
@@ -6,7 +6,18 @@ public class BaseLogicProcessor implements LogicProcessor {
 
     @Override
     public void run(LogicNode logic) {
+        // return INPUT (3) + CONST (8)
         System.out.println("Proceed logic");
         System.out.println(logic);
     }
+
+
+//    private void runE xpression(LogicNode node) {
+//        switch (node.getType()) {
+//            case RETURN -> {
+//                runExpression()
+//            }
+//        }
+//    }
+
 }
